041ed3c0e7d246495fc92107f5c77605fc248d1b
Idan Lev <idan.93lev@gmail.com>
2022-01-09 13:03:16 +0200

    first commit for node js excersises

---
+++ 1-1/1-1.js
@@ -0,0 +1,16 @@
+// Write in your own words the following:
+// What are 3 things node.js has that the client side javascript doesn’t
+// have. And the reasons why.
+// What are 3 things the client side javascript has that node.js doesn’t
+// have. And the reasons why.
+
+// node JS:
+// 1) global object - there is'nt a global object in browser app
+// 2) process - is specific object to node js
+// 3) 
+
+// client side js:
+// 1) window object - node js does'nt run on browser apps
+// 2) document - same reason above^^^, there is no DOM outside the browser
+// 3)
+console.log("hello node,js");
