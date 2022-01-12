1e6542931a0161f33879d6ae695282c7354ddb94
Idan Lev <idan.93lev@gmail.com>
2022-01-12 15:23:38 +0200

    Export-Crud

---
+++ export-crud/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+// app.use(express.json());
+const app = express();
+const port = 4000;
+const arr = [1,2,3,4,5,6];
+
+app.get("/numbers", (req, res) => res.status(200).send(arr));
+
+app.post("/numbers/:num", (req, res) => {
+  const { num } = req.params;
+  arr.push(+num);
+  res.status(201).send(arr);
+});
+
+app.delete("/numbers/:num", (req, res) => {
+  const { num } = req.params;
+  const index = arr.findIndex(number => number === +num);
+  arr.splice(index, 1);
+  res.status(202).send(arr)
+});
+
+app.put("/numbers/:num", (req, res) => {
+  const { num } = req.params;
+  const { newNum } = req.query;
+  const index = arr.findIndex(number => number === +num);
+  arr[index] = +newNum;
+  res.status(202).send(arr)
+});
+
+app.listen(4000, () => {
+  console.log(`App running on port ${port}`);
+});
