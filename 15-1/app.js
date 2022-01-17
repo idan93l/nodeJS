e68f368049c695f8645440ce433f121ed0a89cbf
Idan Lev <idan.93lev@gmail.com>
2022-01-17 22:36:16 +0200

    15-1

---
+++ 15-1/app.js
@@ -0,0 +1,49 @@
+[
+  {
+    _id: ObjectId("61e56ec7b13ae5d9754ebf83"),
+    name: 'Simta',
+    address: {
+      city: 'Tel Aviv',
+      street: 'Dizenoff',
+      coordinates: [ -77, 564, 40.677 ]
+    },
+    type: 'Bar',
+    kosher: false,
+    reviews: [ { date: '13-9-21', score: 10 } ]
+  },
+  {
+    _id: ObjectId("61e5725bb13ae5d9754ebf84"),
+    name: 'Mexicana',
+    address: {
+      city: 'Tel Aviv',
+      street: 'Bugrashov',
+      coordinates: [ -77, 564, 40.677 ]
+    },
+    type: 'Mexican',
+    kosher: false,
+    reviews: [ { date: '24-12-21', score: 8 } ]
+  },
+  {
+    _id: ObjectId("61e5729eb13ae5d9754ebf85"),
+    name: 'Moon',
+    address: {
+      city: 'Tel Aviv',
+      street: 'Bugrashov',
+      coordinates: [ -77, 564, 40.677 ]
+    },
+    type: 'Asian',
+    kosher: false,
+    reviews: [ { date: '10-10-21', score: 7 } ]
+  },
+  {
+    _id: ObjectId("61e57333b13ae5d9754ebf87"),
+    name: 'Nekuda',
+    address: {
+      city: 'Givatayim',
+      coordinates: [ -77, 564, 40.677 ]
+    },
+    type: 'Bar',
+    kosher: false,
+    reviews: [ { date: '19-10-21', score: 5 } ]
+  }
+]
