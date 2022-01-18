ce2c67f5cd314f142cda97958b766fc4ef3572d8
Idan Lev <idan.93lev@gmail.com>
2022-01-19 01:20:25 +0200

    17-1

---
+++ 17-1/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Product = require("./Product.js");
+
+mongoose.connect(
+  "mongodb://localhost/e-commerce-site",
+  // { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("connected");
+  },
+  (e) => console.error(e)
+);
+
+
+async function run() {
+  try {
+    const product = await Product.create({
+      name: "musicman stingray",
+      category: "bass",
+      isActive: true,
+      details: {
+        description: "musicman classic",
+        price: 9500,
+        discount: 10,
+        images: ["image1", "image2"],
+        phone: "0582985326",
+      },
+    });
+    console.log(product);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+run()
